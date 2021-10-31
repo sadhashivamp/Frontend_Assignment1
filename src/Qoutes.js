2010034e11ca7f80c2fa132337d110eb8aaf86d2
sadhashivamp <ramulu320p@gmail.com>
2021-10-31 10:28:05 +0530

    Qoutes page render when user Ckick symbol

--- src/Qoutes.js
+++ src/Qoutes.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./Stocks.css";
 
 export const Qoutes = () => {
+  useEffect(() => {
+    axios
+      .get("https://prototype.sbulltech.com/api/v2/quotes/MRF")
+      .then((response) => console.log(response.data.payload));
+  }, []);
+
   return (
     <div className="stocks">
       <h1>Qoutes page</h1>
